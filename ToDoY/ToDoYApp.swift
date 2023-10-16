47d38875be764ee652e87afcc22d9a857c0b2b91
Hakan Kaya <hakaya@outlook.com>
2023-10-16 15:59:24 +0300

    Initial Commit

---
+++ ToDoY/ToDoYApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoYApp.swift
+//  ToDoY
+//
+//  Created by Hakan Kaya on 16.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
